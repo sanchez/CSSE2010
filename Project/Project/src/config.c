ed26e1e8bdf78db98777cec06b2d5d5a1bd751cd
Daniel Fitz <dfitz360@gmail.com>
2019-05-05 21:49:41 +1000

    Got it working on the mac and fixed up the config option

--- Project/Project/src/config.c
+++ Project/Project/src/config.c
@@ -7,7 +7,7 @@ void init_config() {
 uint8_t config_get(uint8_t c) {
 	switch (c) {
 		case CONFIG_BUZZER_ENABLE:
-			return !!(PINA & (1 << 7));
+			return !(PINA & (1 << 7));
 		default: return 0;
 	}
 }
