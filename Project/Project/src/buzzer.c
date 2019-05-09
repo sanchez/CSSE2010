1872fbd3789ef1c12c76cf92641b55cdee31c1b4
Daniel Fitz <dfitz360@gmail.com>
2019-05-09 19:27:13 +1000

    Started on #8

--- Project/Project/src/buzzer.c
+++ Project/Project/src/buzzer.c
@@ -12,6 +12,11 @@ void init_buzzer() {
 	for (uint8_t i = 0; i < BUZZER_MAX_TONES; i++) {
 		tones[i].tone = 0;
 	}	
+
+	DDRD |= (1 << 4) | (1 << 5);
+	TCCR1A = (0 << COM1A1) | (1 << COM1A0) | (0 << COM1B1) | (0 << COM1B0);
+	TCCR1B = (1 << WGM12) | (0 << CS12) | (1 << CS11) | (0 << CS10);
+	OCR1A = 0;
 }
 
 void buzzer_add_tone(uint8_t t, uint8_t duration) {
@@ -36,30 +41,35 @@ void buzzer_startup() {
 void buzzer_gameover() {
 	addingPos = 0;
 	playingPos = 0;
-	buzzer_add_tone(1, 200);
-	buzzer_add_tone(0, 200);
-	buzzer_add_tone(2, 200);
-	buzzer_add_tone(0, 200);
-	buzzer_add_tone(3, 200);
+	buzzer_add_tone(10, 200);
 	buzzer_add_tone(0, 200);
 	buzzer_add_tone(4, 200);
 	buzzer_add_tone(0, 200);
-	buzzer_add_tone(10, 200);
+	buzzer_add_tone(3, 200);
+	buzzer_add_tone(0, 200);
+	buzzer_add_tone(2, 200);
+	buzzer_add_tone(0, 200);
+	buzzer_add_tone(1, 200);
 }
 
 void buzzer_up() {
 	addingPos = 0;
 	playingPos = 0;
-	buzzer_add_tone(1, 10);
-	buzzer_add_tone(2, 10);
 	buzzer_add_tone(3, 10);
+	buzzer_add_tone(2, 10);
+	buzzer_add_tone(1, 10);
 }
 
-uint32_t buzzerCounter = 0;
 void task_buzzer() {
-	if (!config_get(CONFIG_BUZZER_ENABLE)) return;
+	if (!config_get(CONFIG_BUZZER_ENABLE)) {
+		OCR1A = 0;
+		return;
+	}
 	
-	if (playingPos >= BUZZER_MAX_TONES) return;
+	if (playingPos >= BUZZER_MAX_TONES) {
+		OCR1A = 0;
+		return;
+	}
 	BuzzerValue* t = tones + playingPos;	
 	if (t->start == 0) {
 		t->start = millis();
@@ -69,14 +79,11 @@ void task_buzzer() {
 	if (diff >= t->duration) {
 		t->tone = 0;
 		playingPos++;
-		PORTA &= ~(1 << 0);
+		OCR1A = 0;
 		return;
 	}
 	
 	uint8_t tone = t->tone;
-	
-	buzzerCounter++;
-	if (tone != 0 && (buzzerCounter % tone) == 0) {
-		PORTA ^= (1 << 0);
-	}
+
+	OCR1A = tone * 10;
 }
